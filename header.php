f74afc47dd05cb4289482854b14677f4336d2e6d
jskd <jerome.skoda@hotmail.fr>
2017-08-08 22:19:38 +0200

    add mobile menu slidebar

--- header.php
+++ header.php
@@ -12,45 +12,86 @@
     <?php wp_head();?>
   </head>
   <body class="w3-theme-d5">
-    <nav class="container-fluid w3-top  hidden-print padding-0 w3-theme-d2">
-      <div class="container">
-        <div class="row no-gutters justify-content-center hidden-xs-down">
-          <div class="col-sm-2">
-            <a href="#" class="w3-button w3-block">Blog</a>
-          </div>
-          <div class="col-sm-2">
-            <a href="#menu" class="w3-button w3-block">Projets</a>
-          </div>
-          <div class="col-sm-2">
-            <a href="#about" class="w3-button w3-block">Tutoriels</a>
-          </div>
-          <div class="col-sm-2">
-            <a href="#where" class="w3-button w3-block">Contact</a>
-          </div>
-          <div class="col-sm-1">
-            <a href="#" class="w3-button w3-block">
-              <i class="fa fa-search" aria-hidden="true"></i>
-            </a>
-          </div>
-        </div>
 
-        <!-- Extra small nav -->
-        <div class="row no-gutters justify-content-center hidden-sm-up">
-          <div class="col-8">
-            <a href="#" class="w3-button w3-block w3-black nav-item">
-              <i class="fa fa-bars" aria-hidden="true"></i> Menu
-            </a>
+
+<!-- Sidebar -->
+<nav class="w3-sidebar w3-bar-block w3-animate-left w3-theme-d1" style="display:none;z-index:4" id="mySidebar">
+  <button class="w3-bar-item w3-button  w3-theme-l1" onclick="w3_close()">
+    <i class="fa fa-times" aria-hidden="true"></i> Fermer
+  </button>
+  <a href="#" class="w3-bar-item w3-button">Blog</a>
+  <a href="#" class="w3-bar-item w3-button">Projets</a>
+  <a href="#" class="w3-bar-item w3-button">Tutoriels</a>
+  <a href="#" class="w3-bar-item w3-button">Contact</a>
+</nav>
+
+<!-- Page Content -->
+<div class="w3-overlay w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" id="myOverlay"></div>
+
+<script>
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+</script>
+
+
+    <nav class="container-fluid w3-top  hidden-print padding-0">
+      <div class="row w3-theme-d2">
+        <div class="container">
+          <div class="row no-gutters justify-content-center hidden-xs-down">
+            <div class="col-sm-2">
+              <a href="#" class="w3-button w3-block w3-padding">Blog</a>
+            </div>
+            <div class="col-sm-2">
+              <a href="#menu" class="w3-button w3-block w3-padding">Projets</a>
+            </div>
+            <div class="col-sm-2">
+              <a href="#about" class="w3-button w3-block w3-padding">Tutoriels</a>
+            </div>
+            <div class="col-sm-2">
+              <a href="#where" class="w3-button w3-block w3-padding">Contact</a>
+            </div>
+            <div class="col-sm-1">
+              <a href="#" class="w3-button w3-block w3-padding">
+                <i class="fa fa-search" aria-hidden="true"></i>
+              </a>
+            </div>
           </div>
-          <div class="col-4">
-            <a href="#" class="w3-button w3-block w3-black nav-item">
-              <i class="fa fa-search" aria-hidden="true"></i>
-            </a>
+
+          <!-- Extra small nav -->
+          <div class="row no-gutters justify-content-center hidden-sm-up">
+            <div class="col-8">
+              <a href="#" onclick="w3_open()" class="w3-button w3-block w3-padding">
+                <i class="fa fa-bars" aria-hidden="true"></i> Menu
+              </a>
+            </div>
+            <div class="col-4">
+              <a href="#" class="w3-button w3-block w3-padding">
+                <i class="fa fa-search" aria-hidden="true"></i>
+              </a>
+            </div>
+          </div>
+        </div> <!-- Container -->
+      </div> <!-- Row -->
+
+      <div class="row w3-theme-d3">
+        <div class="container">
+          <div class="row no-gutters justify-content-center">
+            <div class="col-12 w3-padding">
+              <input type="text" class="w3-bar-item w3-input" placeholder="Rechercher">
+            </div>
           </div>
         </div>
       </div>
     </nav>
 
-    <header class="container-fluid w3-theme-d5">
+    <header class="container-fluid w3-theme-d4">
       <div id="plax-zone">
         <img id="earth" src="<?php echo get_theme_file_uri() ?>/assets/images/header/header.jpg" alt="earth web banner">
         <img class="cubesat" src="<?php echo get_theme_file_uri() ?>/assets/images/cubesat/cubesat-0.png" alt="cubesat 0">
@@ -66,11 +107,11 @@
       </div>
       <div class="container header-container">
         <div class="w3-display-container" style="height: 100%">
-          <div class="w3-display-middle w3-text-white" style="width: 100%;">
+          <div class="w3-display-middle" style="width: 100%;">
             <span class="w3-xxlarge">Jérôme Skoda</span><br>
             <span class="w3-xlarge">Passionné d'informatique et de nanosatellite</span>
           </div>
-          <div class="w3-display-bottommiddle w3-left w3-right-align w3-text-white w3-xxlarge" style="width: 100%">
+          <div class="w3-display-bottommiddle w3-left w3-right-align w3-xxlarge" style="width: 100%">
             <i class="fa fa-github-square" aria-hidden="true"></i>
             <i class="fa fa-linkedin-square" aria-hidden="true"></i>
             <i class="fa fa-rss-square" aria-hidden="true"></i>
@@ -80,5 +121,5 @@
       </div>
     </header>
 
-    <main class="container-fluid hide-overflow w3-theme-l5">
-      <div class="container content">
+    <main class="container-fluid w3-theme-l5">
+      <div class="container">
