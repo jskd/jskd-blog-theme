b977371f151473d2a62a7457496f3d9243a0364a
jskd <jerome.skoda@hotmail.fr>
2017-08-29 22:33:56 +0200

    fix placeholder search

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <div class="container" style="margin: 0; padding: 0;">
   <form class="row no-gutters justify-content-center"  role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
     <div class="col-8">
-      <input class="w3-input" required placeholder="Chercher" value="" name="s" id="s" type="text">
+      <input class="w3-input" required placeholder="Recherche" value="" name="s" id="s" type="text">
     </div>
     <div class="col-3 col-sm-2 col-md-1 col-lg-1">
       <button type="submit" class="w3-button w3-block w3-theme-d5" >
