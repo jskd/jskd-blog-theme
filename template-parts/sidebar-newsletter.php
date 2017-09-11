36793a24063478c25d939ac7e3927e069e64d696
jskd <jerome.skoda@hotmail.fr>
2017-09-11 14:29:59 +0200

    quick customize newsletter sidebar

--- template-parts/sidebar-newsletter.php
+++ template-parts/sidebar-newsletter.php
@@ -2,36 +2,55 @@
     <header class="w3-container w3-theme-d1">
       <h3>Newsletter</h3>
     </header>
+
 <div class="w3-container" >
-      <div class="w3-section">
-        Restez informés en vous inscrivant à la newsletter
-      </div>
-    <form  class="w3-section" id="newsletter-sidebar-form" method="post" action="/?na=s">
-        <div class="w3-row">
-          <div class="w3-col" style="width:50px">
-            <i class="w3-xxlarge fa fa-envelope-o"></i>
-          </div>
-          <div class="w3-rest">
-            <input class="w3-input w3-hover-theme w3-theme-l4 w3-border-0" placeholder="Entrer son email" name="ne" type="email" required placeholder="Email">
-          </div>
-        </div>
-    </form>
+<?php 
+
+$doc = new DOMDocument;
+
+// Load mailpoet with php UTF-8 magic bullshit
+$doc->loadHTML(
+  mb_convert_encoding(
+    do_shortcode('[mailpoet_form id="1"]') , 'HTML-ENTITIES', 'UTF-8'
+  ),
+  LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+
+$finder = new DomXPath($doc);
+
+// Customize: Green sumit button
+$items = $finder->query("//*[contains(@class, 'mailpoet_submit')]");
+$button_summit= $items->item(0);
+addClassDOMElement($button_summit, 'w3-button w3-green w3-margin-right');
+
+// Customize: Add gray footer
+$p_summit = $button_summit->parentNode;
 
 
-       <div class="w3-section">
-         <div id="newsletter-sidebar-error" class="w3-panel w3-pale-red w3-leftbar w3-border w3-border-red w3-padding">
-           L'adresse de messagerie n'est pas valide.
-         </div>
-         <div id="newsletter-sidebar-sucess" class="w3-panel w3-pale-green w3-leftbar w3-border w3-border-green w3-padding">
-           Merci, vous êtes abonné à la newsletter. <br>
-           Vous recevrez un e-mail de confirmation dans quelques minutes. 
-           Suivez le lien pour confirmer votre abonnement. <br>
-           Si le courrier électronique prend plus de 15 minutes pour apparaître dans 
-           votre messagerie, vérifiez votre dossier de spam.
-         </div>
-       </div>
+// Move button on bottom
+$form= $p_summit->parentNode;
+$form->appendChild($p_summit);
 
-      <div class="w3-section" id="newsletter-sidebar-submit" >
-        <button class="w3-button w3-green" onclick="submit_newsletter_sidebar()">S'abonner</button>
+// Customize: Change style of email input
+$items = $finder->query("//input[contains(@class, 'mailpoet_text')]");
+$email_input= $items->item(0);
+addClassDOMElement($email_input, 'w3-input w3-hover-theme w3-theme-l4 w3-border-0');
+
+// Customize: Fix margin of <p> input
+$p_input= $email_input->parentNode;
+addClassDOMElement($p_input, 'w3-margin-top');
+
+// Customize: Fix margin of <div> message
+$items = $finder->query("//div[contains(@class, 'mailpoet_message')]");
+$message= $items->item(0);
+addClassDOMElement($message, 'w3-margin-top');
+
+echo $doc->saveHTML();
+
+?>
+
+
+
+
+
       </div>
 
