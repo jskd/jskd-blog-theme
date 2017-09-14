b8c8f234a538e492bfcf7747f9b781c380f18c7a
jskd <jerome.skoda@hotmail.fr>
2017-09-14 23:27:51 +0200

    add projet shortcode

--- page.php
+++ page.php
@@ -30,7 +30,7 @@ Tags: blog bootstrap
 			get_template_part( 'content', 'page' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
+			if ( comments_open() || get_comments_number() > 0 ) :
 				comments_template();
 			endif;
 
