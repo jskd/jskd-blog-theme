b3fe9f42b099c010a4e3f4d58d3b101c60971337
jskd <jerome.skoda@hotmail.fr>
2017-09-30 21:20:52 +0200

    fix search list

--- search.php
+++ search.php
@@ -12,8 +12,11 @@ Template Name: Search Page
 
 
 <h2>Recherche</h2>
+<p>
 <?php get_search_form(); ?>
 <?php /* Start the Loop */ ?>
+</p>
+<p>
 			<?php // The Query
 global $query_string;
 
@@ -32,22 +35,33 @@ $the_query = new WP_Query($search_query);
 
 // The Loop
 if ( $the_query->have_posts() ) {
-	echo '<ul>';
+
+?>
+<ul>
+<?php
+
+
 	while ( $the_query->have_posts() ) {
 		$the_query->the_post();?>
-
+<li>
 <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-
+</li>
 <?php
-		echo '<li>' . get_the_title() . '</li>';
-	}
-	echo '</ul>';
+  }
+?>
+</ul>
+<?php
+
+
+
+
 	/* Restore original Post Data */
 	wp_reset_postdata();
 } else {
 	echo "Aucun résultat";
 }
  ?>
+</p>
 		</main>
 
 		<?php get_sidebar(); ?>
